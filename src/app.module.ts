284329949aabba9ae5302da17c59e195b71262b4
joaoefornazari <joaoefornazari@gmail.com>
2025-03-07 13:10:37 -0300

    feat: configurações de banco de dados baseadas no ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DatabaseConfig from './config/database.config';
 
 @Module({
   imports: [
-		ConfigModule.forRoot({ isGlobal: true })
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRoot(DatabaseConfig.get())
 	],
   controllers: [AppController],
   providers: [AppService],
