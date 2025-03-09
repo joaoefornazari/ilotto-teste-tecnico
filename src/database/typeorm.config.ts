213737909167144225e3a7fbdbafe05aa2317fa1
joaoefornazari <joaoefornazari@gmail.com>
2025-03-09 15:44:29 -0300

    feat: valores default para configuração do TypeORM

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -2,13 +2,13 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm"
 
 const TypeORMConfig = {
 	type: 'postgres',
-	host: process.env.DB_HOST,
-	port: Number(process.env.DB_PORT),
+	host: process.env.DB_HOST ?? 'localhost',
+	port: Number(process.env.DB_PORT) ?? 5432,
 	username: process.env.DB_USER ?? 'postgres',
 	password: process.env.DB_PASSWORD ?? 'root',
-	database: process.env.DB_NAME,
-	entities: ["src/**/**.entity.ts"],
-	migrations: ["src/database/migrations/**{.ts,.js}"],
+	database: process.env.DB_NAME ?? 'test',
+	entities: ["dist/**/**.entity.js"],
+	migrations: ["dist/database/migrations/**{.js}"],
 	synchronize: true,
 } satisfies TypeOrmModuleOptions
 
