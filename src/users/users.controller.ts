63e4be70d7df0c079748d794f1df85d4c689e0f5
joaoefornazari <joaoefornazari@gmail.com>
2025-03-11 16:08:41 -0300

    doc: endpoint de login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,13 +2,25 @@ import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserLoginDto } from './dto/user-login.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import generateDocs from 'src/docs';
 
 @Controller('users')
 export class UsersController {
 	constructor(private usersService: UsersService) {}
 
+	/**
+	 * Faz login do usuário.
+	 *  
+	 * @remarks Permite que o usuário faça login com email e senha.
+	 */
+	@ApiResponse(generateDocs('sign-in', HttpStatus.CREATED))
+	@ApiResponse(generateDocs('sign-in', HttpStatus.NOT_FOUND))
+	@ApiResponse(generateDocs('sign-in', HttpStatus.UNAUTHORIZED))
+	@ApiResponse(generateDocs('sign-in', HttpStatus.INTERNAL_SERVER_ERROR))
 	@Post('sign-in')
-	signIn(@Body() userLoginDto: UserLoginDto) {
+	signIn(@Body() userLoginDto: UserLoginDto): Promise<{token: string}> {
+		console.log(generateDocs('sign-in', HttpStatus.CREATED))
 		return this.usersService.checkUserCredentials(userLoginDto)
 	}
 
