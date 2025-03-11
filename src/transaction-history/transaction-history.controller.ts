f767f25b46c1004f9658d645709c4e56d8663c4d
joaoefornazari <joaoefornazari@gmail.com>
2025-03-11 20:36:37 -0300

    feat: docs dos endpoints de transaçõe

--- src/transaction-history/transaction-history.controller.ts
+++ src/transaction-history/transaction-history.controller.ts
@@ -1,23 +1,52 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { TransactionHistoryService } from './transaction-history.service';
 import { TransactionDto } from './dto/transaction.dto';
 import { TransferDto } from './dto/transfer.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import generateDocs from 'src/docs';
 
 @Controller('transactions')
 export class TransactionHistoryController {
 	constructor(private transactionService: TransactionHistoryService) {}
 
+	/**
+	 * Deposita um valor.
+	 * 
+	 * @remarks Deposita um valor na conta do usuário logado.
+	 */
+	@ApiResponse(generateDocs('deposit', HttpStatus.OK))
+	@ApiResponse(generateDocs('deposit', HttpStatus.BAD_REQUEST))
+	@ApiResponse(generateDocs('deposit', HttpStatus.INTERNAL_SERVER_ERROR))
 	@Post('deposit')
+	@HttpCode(HttpStatus.OK)
 	async deposit(@Body() depositDto: TransactionDto) {
 		return await this.transactionService.addMoney(depositDto)
 	}
 
+	/**
+	 * Faz o saque de um valor.
+	 * 
+	 * @remarks Saca um valor da conta do usuário logado.
+	 */
+	@ApiResponse(generateDocs('withdraw', HttpStatus.OK))
+	@ApiResponse(generateDocs('withdraw', HttpStatus.BAD_REQUEST))
+	@ApiResponse(generateDocs('withdraw', HttpStatus.INTERNAL_SERVER_ERROR))
 	@Post('withdraw')
+	@HttpCode(HttpStatus.OK)
 	async withdraw(@Body() withdrawDto: TransactionDto) {
 		return await this.transactionService.subtractMoney(withdrawDto)
 	}
 
+	/**
+	 * Transfere um valor entre contas.
+	 * 
+	 * @remarks Transfere um valor da conta do usuário logado para o ID informado. 
+	 */
+	@ApiResponse(generateDocs('transfer', HttpStatus.OK))
+	@ApiResponse(generateDocs('transfer', HttpStatus.BAD_REQUEST))
+	@ApiResponse(generateDocs('transfer', HttpStatus.INTERNAL_SERVER_ERROR))
 	@Post('transfer')
+	@HttpCode(HttpStatus.OK)
 	async transfer(@Body() transferDto: TransferDto) {
 		return await this.transactionService.transferMoney(transferDto)
 	}
