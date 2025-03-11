9c37215fbd6af7debad674a3bd14e3e8d2bf5900
joaoefornazari <joaoefornazari@gmail.com>
2025-03-11 13:32:51 -0300

    feat: atualizando DTO para manusear userId vindo da token

--- src/transaction-history/dto/transaction.dto.ts
+++ src/transaction-history/dto/transaction.dto.ts
@@ -1,6 +1,9 @@
-import { IsNumber } from "class-validator";
+import { IsString, IsNumber } from "class-validator";
 
 export class TransactionDto {
 	@IsNumber()
 	value: number
+
+	@IsString()
+	userId: string
 }
