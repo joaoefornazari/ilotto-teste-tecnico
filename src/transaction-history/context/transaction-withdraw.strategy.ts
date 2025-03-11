8b9d9ca4aee707ddd837610d184a0166b98c60fa
joaoefornazari <joaoefornazari@gmail.com>
2025-03-10 23:28:29 -0300

    fix: corrigindo query de saque para apenas autorizá-lo qando for menor que o saldo atual

--- src/transaction-history/context/transaction-withdraw.strategy.ts
+++ src/transaction-history/context/transaction-withdraw.strategy.ts
@@ -4,7 +4,7 @@ import { InternalServerErrorException } from "@nestjs/common";
 
 class TransactionWithdraw implements TransactionOperationStrategy {
 	
-	private userId: string
+	private userId!: string
 
 	constructor(
 		private dataSource: DataSource,
@@ -15,21 +15,32 @@ class TransactionWithdraw implements TransactionOperationStrategy {
 
 	async execute(value: number) {
 		const queryRunner = this.dataSource.createQueryRunner()
-		try {	
+		try {
 			await queryRunner.connect()
 		} catch (error) {
 			queryRunner.release()
-			throw new InternalServerErrorException('Database error.')
+			throw new InternalServerErrorException(`Database error: ${error}`)
 		}
 
 		await queryRunner.startTransaction()
 
 		try {
-			await queryRunner.query('UPDATE users SET balance = balance - ? WHERE id = ?', [value, this.userId])
+			const result = await queryRunner.query(`
+				UPDATE users 
+				SET balance = CASE 
+					WHEN balance >= ${value} THEN balance - ${value} 
+					ELSE balance 
+				END
+				WHERE id = '${this.userId}' AND balance >= ${value}
+			`)
+			
+			if (!result[1]) {
+				throw new InternalServerErrorException(`Invalid payload.`)
+			}
 			await queryRunner.commitTransaction()
 		} catch (error) {
 			await queryRunner.rollbackTransaction()
-			throw new InternalServerErrorException('Database error.')
+			throw new InternalServerErrorException(`Database error: ${error}`)
 		} finally {
 			queryRunner.release()
 		}
