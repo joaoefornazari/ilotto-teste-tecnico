1643eeaf6ce81d9d82fdf26841046de9bd9dae97
joaoefornazari <joaoefornazari@gmail.com>
2025-03-12 20:00:41 -0300

    feat: função para obter formato de data desejado no banco de dados

--- src/transaction-history/transaction-history.utils.ts
+++ src/transaction-history/transaction-history.utils.ts
@@ -1,3 +1,11 @@
 export const validateValue = (value: number) => {
 	return value >= 20.0 && value <= 10000.00
 }
+
+export const getCurrentYearMonthDateString = (date: Date) => {
+	const year = date.getFullYear()
+	const month = String(date.getMonth() + 1).padStart(2, '0')
+	const day = String(date.getDate()).padStart(2, '0')
+
+	return `${year}/${month}/${day}`
+}
