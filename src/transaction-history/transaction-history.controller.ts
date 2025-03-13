7e58516a90404b4fb146bd722600ed4ac855d9a7
joaoefornazari <joaoefornazari@gmail.com>
2025-03-13 00:33:45 -0300

    feat: headers nos docs do swagger

--- src/transaction-history/transaction-history.controller.ts
+++ src/transaction-history/transaction-history.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { TransactionHistoryService } from './transaction-history.service';
 import { TransactionDto } from './dto/transaction.dto';
 import { TransferDto } from './dto/transfer.dto';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiHeaders, ApiResponse, ApiBody } from '@nestjs/swagger';
 import generateDocs from 'src/docs';
 
 @Controller('transactions')
@@ -14,6 +14,7 @@ export class TransactionHistoryController {
 	 * 
 	 * @remarks Deposita um valor na conta do usuário logado.
 	 */
+	@ApiHeaders([{ name: 'authorization' }])
 	@ApiResponse(generateDocs('deposit', HttpStatus.OK))
 	@ApiResponse(generateDocs('deposit', HttpStatus.BAD_REQUEST))
 	@ApiResponse(generateDocs('deposit', HttpStatus.INTERNAL_SERVER_ERROR))
@@ -28,6 +29,7 @@ export class TransactionHistoryController {
 	 * 
 	 * @remarks Saca um valor da conta do usuário logado.
 	 */
+	@ApiHeaders([{ name: 'authorization' }])
 	@ApiResponse(generateDocs('withdraw', HttpStatus.OK))
 	@ApiResponse(generateDocs('withdraw', HttpStatus.BAD_REQUEST))
 	@ApiResponse(generateDocs('withdraw', HttpStatus.INTERNAL_SERVER_ERROR))
@@ -42,6 +44,7 @@ export class TransactionHistoryController {
 	 * 
 	 * @remarks Transfere um valor da conta do usuário logado para o ID informado. 
 	 */
+	@ApiHeaders([{ name: 'authorization' }])
 	@ApiResponse(generateDocs('transfer', HttpStatus.OK))
 	@ApiResponse(generateDocs('transfer', HttpStatus.BAD_REQUEST))
 	@ApiResponse(generateDocs('transfer', HttpStatus.INTERNAL_SERVER_ERROR))
